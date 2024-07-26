42b52626fc6c4230017887be776d9d4393c76f6d
arata-exe <aommine1@gmail.com>
2024-07-26 09:12:45 +0700

    update

--- backend/src/app/api/route.js
+++ backend/src/app/api/route.js
@@ -13,7 +13,7 @@ client.connect();
 
 export async function GET() {
   try {
-    const result = await client.query('SELECT * FROM tbl_student');
+    const result = await client.query('SELECT * FROM tbl_users');
     return NextResponse.json(result.rows);
   } catch (err) {
     return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
