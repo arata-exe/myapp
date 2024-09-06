4479b7662c9465b350ea51894f860211c4ad5630
arata-exe <aommine1@gmail.com>
2024-09-06 19:34:28 +0700

    gk

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -20,11 +20,12 @@ export default function Page() {
         console.error('Error fetching data:', error);
       }
     }
- 
-  getUsers()
-  const interval  = setInterval(getUsers, 1000);
-  return () => clearInterval(interval );
-}, []);
+  
+    getUsers();
+    const interval = setInterval(getUsers, 1000);
+    return () => clearInterval(interval); // Ensure cleanup function is returned correctly
+  }, []);
+  
 
 const handleDelete = async (id) => {
   // Include the user ID in the confirmation message
@@ -54,39 +55,46 @@ const handleDelete = async (id) => {
     <Navbar />
     <br /><br /><br /><br />
     <div className="container">
-      <div class="card">
-  <div class="card-header">
-    Users List
-  </div>
-  <div class="card-body">
-  <div className="row">
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th className='col-md-2 text-center'>#</th>
-            <th className='col-md-4'>Firstname</th>
-            <th className='col-md-4'>Lastname</th>
-            <th className='col-md-1'>Edit</th>
-            <th className='col-md-1'>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item.id}>
-              <td className='text-center'>{item.id}</td>
-              <td>{item.firstname}</td>
-              <td>{item.lastname}</td>
-              <td><Link href={`/users/edit/${item.id}`} className="btn btn-warning">Edit</Link></td>
-              <td><button className="btn btn-pill btn-danger" type="button" onClick={() => handleDelete(item.id)}><i class="fa fa-trash"></i>Del</button></td>
-            </tr>
-          ))}
-        </tbody>
+    <div className="card">
+      <div className="card-header">
+        Users List
+      </div>
+      <div className="card-body">
+        <div className="row">
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th className='col-md-2 text-center'>#</th>
+                <th className='col-md-4'>Firstname</th>
+                <th className='col-md-4'>Lastname</th>
+                <th className='col-md-1'>Edit</th>
+                <th className='col-md-1'>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td className='text-center'>{item.id}</td>
+                  <td>{item.firstname}</td>
+                  <td>{item.lastname}</td>
+                  <td>
+                    <Link href={`/users/edit/${item.id}`} className="btn btn-warning">
+                      Edit
+                    </Link>
+                  </td>
+                  <td>
+                    <button className="btn btn-pill btn-danger" type="button" onClick={() => handleDelete(item.id)}>
+                      <i className="fa fa-trash"></i> Del
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 
-      </table>
-    </div>
-    </div>
-
-    </div>
     </div>
     <br /><br />
 
